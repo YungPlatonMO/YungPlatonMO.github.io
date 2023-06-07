ee998156f5198aa8171b8e8fb958af7eba3ca854
YungPlatonMO <126783525+YungPlatonMO@users.noreply.github.com>
2023-06-07 14:24:03 +0300

    1

--- JS4/lab46.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-function getSecondsToday() {
-    let d = new Date();
-    return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
-  }
-console.log(`С начала дня прошло ${getSecondsToday()} секунд`)
