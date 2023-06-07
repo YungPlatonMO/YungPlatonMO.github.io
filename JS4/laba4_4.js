89a5ee5378078dcdeac55e94af5acd9033ff3c07
YungPlatonMO <126783525+YungPlatonMO@users.noreply.github.com>
2023-06-07 19:49:11 +0300

    1

--- JS4/laba4_4.js
+++ JS4/laba4_4.js
@@ -1,21 +1,6 @@
+'use strict'
 function getSecondsToday() {
-    let dataDay = new Date();
-    return dataDay.getHours() * 3600 + dataDay.getMinutes() * 60 + dataDay.getSeconds();
-}
-console.log(getSecondsToday())
-
-function formatDate(date) {
-
-    let day = date.getDate();
-    if (day < 10) day = '0' + day;
-
-    let mes = date.getMonth() + 1;
-    if (mes < 10) mes = '0' + mes;
-
-    let year = date.getFullYear() % 100;
-    if (year < 10) year = '0' + year;
-
-    return day + '.' + mes + '.' + year;
-}
-d = new Date(2022, 9, 25);
-console.log(formatDate(d))
+    let d = new Date();
+    return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+  }
+console.log(`С начала дня прошло ${getSecondsToday()} секунд`)
